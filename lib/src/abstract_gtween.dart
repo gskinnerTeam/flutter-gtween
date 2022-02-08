8b0473a0c962a29aba2fbc6fc29bb0ac0756a3ba
Shawn <shawn@gskinner.com>
2022-02-08 09:30:56 -0700

    Add tween method to all tweens, to enable syntax GTween.tween(someChild)

--- lib/src/abstract_gtween.dart
+++ lib/src/abstract_gtween.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:gtween/gtween.dart';
 
 abstract class GTween<T> {
   GTween({this.curve, required this.from, required this.to});
@@ -8,6 +9,10 @@ abstract class GTween<T> {
   final T to;
 
   Widget build(BuildContext context, Widget child, Animation<double> anim);
+
+  /// Allows any tween to be used to initiate a tween:
+  ///   GFade().tween(Logo());
+  GTweener tween(Widget child, {Key? key}) => GTweener([this], child: child, key: key);
 
   /// Wraps a CurvedAnimation if a curve is defined, returns original anim if not
   Animation<double> applyCurve(Animation<double> anim) =>
